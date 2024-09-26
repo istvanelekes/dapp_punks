ebd9465849ef3dc251b2757c019ae9b47ba16f0e
Istvan Elekes <istvan.elekes@partner.rolls-roycemotorcars.com>
2024-09-26 14:36:13 +0300

    Mint multiple NFTs on website

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import InputGroup from "react-bootstrap/InputGroup"
 import { Spinner } from "react-bootstrap"
 
 const Mint = ({ provider, nft, cost, isWhitelisted, setIsLoading }) => {
 
     const [isWaiting, setIsWaiting] = useState(false)
+    const [amount, setAmount] = useState(0)
 
     const mintHandler = async (e) => {
         e.preventDefault()
@@ -13,7 +15,7 @@ const Mint = ({ provider, nft, cost, isWhitelisted, setIsLoading }) => {
         
         try {
             const signer = await provider.getSigner()
-            const transaction = await nft.connect(signer).mint(1, { value: cost })
+            const transaction = await nft.connect(signer).mint(amount, { value: cost.mul(amount) })
             await transaction.wait()
         } catch {
             window.alert('User rejected or transaction reverted')
@@ -27,16 +29,21 @@ const Mint = ({ provider, nft, cost, isWhitelisted, setIsLoading }) => {
             {isWaiting ? (
                 <Spinner animation="border" style={{ display: 'block', margin: '0 auto'}}/>
             ) : (
-                <Form.Group>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        disabled={!isWhitelisted} 
+                        type="number"
+                        placeholder="Mint amount"
+                        onChange={(e) => setAmount(e.target.value)}
+                    />
                     <Button 
                         variant='primary'
                         type='submit'
                         disabled={!isWhitelisted}
-                        style={{ width: '100%'}}
                     >
                         Mint
                     </Button>
-                </Form.Group>
+                </InputGroup>
             )}
         </Form>
     )
